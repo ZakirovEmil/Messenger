5bdf9a4b44eac6bd84c8406f212d8ea2d589faf4
Emil Zakirov <komrima@mail.ru>
2021-05-27 15:47:20 +0500

    created singin win

--- src/main/java/client/gui/MainWin.java
+++ src/main/java/client/gui/MainWin.java
@@ -1,34 +1,48 @@
 package client.gui;
 
+import client.gui.chat.Chat;
+
 import javax.swing.*;
 
 public class MainWin extends JFrame {
 
     private JPanel chatPanel;
     private Chat chat;
+    private String login;
+    private char[] password;
 
 
-    public MainWin(){
+    public MainWin() {
         prepareMainFrame();
     }
 
-    private void prepareMainFrame(){
-        setSize(500, 500);
+    private void prepareMainFrame() {
         setTitle("Messenger");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
         prepareChat();
+        prepareSignIn();
+
+
+        pack();
 
         setVisible(true);
     }
 
-    private void prepareChat(){
+    private void prepareChat() {
         chat = new Chat();
         chatPanel = chat.getChatPanel();
         add(chatPanel);
     }
 
-    public static void main(String[] args){
+    private void prepareSignIn() {
+        var signIn = new SignInWin(this, "SignIn", true);
+    }
+
+    public void setChatNameAndPassword(String name, char[] password) {
+        chat.setName(name);
+    }
+
+    public static void main(String[] args) {
         new MainWin();
     }
 }
